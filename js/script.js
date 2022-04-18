0e5e53157656e329ecfec6132d3a230df1b605d4
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-18 13:46:07 +0200

    created basic markup

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import { slides } from "./data.js";
+
+// new Slider(
+//   slides, // слайды
+//   "slider", // id для вставки в html
+//   {
+//     loop: true,
+//     navs: true,
+//     pags: true,
+//     auto: true,
+//     delay: 3,
+//   }
+// );
+
+console.log(slides);
